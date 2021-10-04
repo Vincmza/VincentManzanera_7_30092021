3ed02e1812b40c5d18b6e61aa340b9964c892e03
Vincent Manzanera <vincent.mzapro@outlook.fr>
2021-10-04 23:44:46 +0200

    get function to get posts and comments related finished

--- backend/src/controllers/posts.js
+++ backend/src/controllers/posts.js
@@ -14,7 +14,7 @@ exports.getAllPosts = (req, res) => {
 //Get one post and all comments about it
 exports.getOnePost = (req, res) => {
     connection
-        .query("SELECT * FROM posts JOIN commentaires ON commentaires.post_id = posts.id", req.params['postId'])
+        .query("SELECT * FROM posts JOIN commentaires ON commentaires.post_id = posts.id WHERE posts.id = ?", req.params['postId'])
         .then((post) => {
             res.json(post);
         })
