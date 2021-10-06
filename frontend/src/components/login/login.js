7175def59df7a4d3613b95298812e24c5446bb6c
Vincent Manzanera <vincent.mzapro@outlook.fr>
2021-10-06 17:20:51 +0200

    login component set up with responsive design

---
+++ frontend/src/components/login/login.js
@@ -0,0 +1,29 @@
+import {FaUserCircle} from "react-icons/fa";
+import React, {useEffect} from 'react';
+import './login.css';
+
+function Login (props){
+     return (
+
+        <form>
+            <div className="userLogo"><FaUserCircle/></div>
+            <div className="emailRelated">
+                <label for="emailAdress" className="formLabel"></label>
+                <input type="email" id="emailAdress" placeholder="Adresse e-mail"></input>
+            </div>
+            <div className="passwordRelated">
+                <label for="password" className="formLabel"></label>
+                <input type="password" id="password" placeholder="Tapez votre mot de passe"></input>
+            </div>
+            <button type="submit" className="loginButton">Connexion</button>
+
+            <p className="signupSentence">Toujours pas inscrit ?<br/> Cliquez sur le bouton en dessous</p>
+        
+            <button type="submit" className="signupButton">S'enregistrer</button>
+            
+        </form>
+
+     )
+};
+
+export default Login;
--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -5,13 +5,15 @@ import React, {useEffect, useState} from 'react';
 //Components
 import Header from '../components/header/header'
 import Footer from '../components/footer/footer'
+import Login from '../components/login/login'
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Login></Login>
       <Footer></Footer>
-    </div>   
+    </div>  
   );
 }
 
