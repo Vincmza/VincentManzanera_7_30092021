70107d2b6ad9089eac8a0f572eb45dd6df19c80d
Vincent Manzanera <vincent.mzapro@outlook.fr>
2021-10-07 17:11:12 +0200

    setup corrections to signup and login style

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Header from "../components/header/header"
 import Footer from "../components/footer/footer"
-import Login from '../components/log/login'
+
 
 const Profile = () => {
     return (
         <div>
             <Header></Header>
-            <Login></Login>
             <Footer></Footer>
         </div>
     );
