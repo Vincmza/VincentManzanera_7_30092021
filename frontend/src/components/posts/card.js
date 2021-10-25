1d640a689a9a55d95640c1f0ec0f7983a4030dc2
Vincent Manzanera <vincent.mzapro@outlook.fr>
2021-10-25 21:11:56 +0200

    set up some modifications about card.js and link component

--- frontend/src/components/posts/card.js
+++ frontend/src/components/posts/card.js
@@ -1,6 +1,7 @@
-import {BiUser} from "react-icons/bi";
+import { BiUser } from "react-icons/bi";
 import React, { useEffect, useState } from "react";
 import { FaSpinner, FaRegComment } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import Like from "./likeButton";
 import "./card.css";
 
@@ -15,40 +16,36 @@ const Card = (props) => {
     }, []);
     return (
         <li className="card_container" key={props.post.post_id}>
-            {isLoading ? <FaSpinner /> : 
-            
-            <>
-            <div className="post_header">
-                <div className="user_icon">
-                    <BiUser/>
-                </div>
-                <div className="user_pseudo">
-                    {props.post.username}
-                </div>
-            </div>
-            <div className="post_body">
-                <div className="post_title">
-                    {props.post.post_title}
-                </div>
-                <div className="post_content">
-                    {props.post.post_content}
-                </div>
-            </div>            
-            <div className="like_tab__button">
-                <button className="like_post" type="submit">
-                    <Like post={props.post}/>
-                </button>
-                <span className="like_number">{props.post.listLikes.length}</span>
-            </div>           
-            <div className="comment_section">
-                <div className="comment_icon">
-                    <FaRegComment/>
-                </div>
-                <span className="comment_numbers">
-                    {props.post.listComment.length}
-                </span>
-            </div>                       
-            </>}
+            {isLoading ? (
+                <FaSpinner />
+            ) : (
+                <>
+                    <Link to={`/posts/${props.post.post_id}`}>
+                        <div className="post_header">
+                            <div className="user_icon">
+                                <BiUser />
+                            </div>
+                            <div className="user_pseudo">{props.post.username}</div>
+                        </div>
+                        <div className="post_body">
+                            <div className="post_title">{props.post.post_title}</div>
+                            <div className="post_content">{props.post.post_content}</div>
+                        </div>
+                        <div className="like_tab__button">
+                            <button className="like_post" type="submit">
+                                <Like post={props.post} />
+                            </button>
+                            <span className="like_number">{props.post.listLikes.length}</span>
+                        </div>
+                        <div className="comment_section">
+                            <div className="comment_icon">
+                                <FaRegComment />
+                            </div>
+                            <span className="comment_numbers">{props.post.listComment.length}</span>
+                        </div>
+                    </Link>
+                </>
+            )}
         </li>
     );
 };
